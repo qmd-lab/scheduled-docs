c726dbd6870bd048b7d3d20bb1707d5cecf40e6e
andrewpbray <andrewbray@berkeley.edu>
2024-04-11 10:08:46 -0700

    switch from config to scheduledDocs

--- _extensions/scheduled-docs/rm-draftlist.ts
+++ _extensions/scheduled-docs/rm-draftlist.ts
@@ -3,10 +3,10 @@
 import { readYML, removeTempDir } from "./scheduled-docs.ts";
 
 // Set parameters
-const configPath = '_quarto.yml';
-const configKey = "scheduled-docs"
+const ymlPath = '_quarto.yml';
+const scheduledDocsKey = "scheduled-docs"
 const tempFilesDir = './scheduled-docs_files';
 
 // Run functions
-const config = await readYML(configPath);
-await removeTempDir(config, configKey, tempFilesDir);
+const scheduledDocs = await readYML(ymlPath, scheduledDocsKey);
+await removeTempDir(scheduledDocs, tempFilesDir);
